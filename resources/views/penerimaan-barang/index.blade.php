66f2367210b5ef07f4a7b270be9262832f427dad
bayuapriansyah <bayuapriansyah2022@gmail.com>
2025-10-04 02:58:14 +0700

    Update index.blade.php

--- resources/views/penerimaan-barang/index.blade.php
+++ resources/views/penerimaan-barang/index.blade.php
@@ -21,8 +21,24 @@
                     <input type="number" name="qty" id="qty" class="form-control" style="width:100px;">
                 </div>
                 <div style="padding-top: 32px;">
-                    <button class="btn btn-dark">Tambahkan</button>
+                    <button class="btn btn-dark" id="btnadd">Tambahkan</button>
                 </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-produk" id="table-produk">
+                    <thead>
+                        <tr>
+                            <th>Produk</th>
+                            <th>Qty</th>
+                            <th>Opsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -60,23 +76,81 @@
                     cache: true
                 }
             });
-        });
 
-        $("#select2").on("change", function (e) {
-            let id = $(this).val();
+            $("#select2").on("change", function (e) {
+                let id = $(this).val();
 
-            $.ajax({
-                type: "GET",
-                url: "{{ route('get-data.cek-stok') }}",
-                data: {
-                    id: id
-                },
-                dataType: "json",
-                success: function (response) {
-                    console.log(response);
-                    $('#jumlah').val(response);
+                $.ajax({
+                    type: "GET",
+                    url: "{{ route('get-data.cek-stok') }}",
+                    data: {
+                        id: id
+                    },
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+                        $('#jumlah').val(response);
+                    }
+                })
+            });
+
+            $("#btnadd").on("click", function () {
+
+                const selectedId = $("#select2").val();
+                const qty = Number($("#qty").val());
+                const jumlah = Number($("#jumlah").val());
+                const produk = $("#select2 option:selected").text().trim();
+
+                if (!selectedId || !qty) {
+                    alert('Produk dan Qty harus diisi');
+                    return;
                 }
-            })
+                if (qty > jumlah) {
+                    alert('Jumlah barang tidak tersedia');
+                    return;
+                }
+
+                let exist = false;
+                $("#table-produk tbody tr").each(function () {
+                    const rowProduk = $(this).find("td:first").text();
+
+                    if (rowProduk === produk) {
+                        let jumlah = Number($(this).find("td:eq(1)").text());
+                        let newQty = Number(qty) + Number(jumlah);
+
+                        $(this).find("td:eq(2)").text(newQty);
+                        exist = true;
+                        return false;
+
+                    }
+                });
+
+                if (!exist) {
+                    const row = `
+                                             <tr>
+                                                <td>${produk}</td>
+                                                 <td>${qty}</td>
+                                                <td>
+                                                <button class="btn btn-danger btn-remove">
+                                                <i class="fas fa-trash"></i></button>
+                                                 </td>
+                                                                 </tr>
+                                                     `
+                    $("#table-produk tbody").append(row);
+                }
+
+                $("#select2").val(null).trigger("change");
+                $("#jumlah").val(null);
+                $("#qty").val(null);
+            });
+
+
+            $("#table-produk").on("click", ".btn-remove", function () {
+                $(this).closest("tr").remove();
+            });
+
         });
+
+
     </script>
 @endpush
